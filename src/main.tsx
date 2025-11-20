1c78da1d4b420e143de706dc556e0e984db8d541
Alice Garnova <alicefedina@icloud.com>
2025-11-20 17:08:32 +0300

    init app

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./App";
-import { Provider } from "react-redux";
-import store from "./store/Store";
 
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
